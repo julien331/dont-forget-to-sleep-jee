d31ff3a851ba19a2358fced1a55cbe1403319c6b
Téo Schaffner <teo.schaffner@he-arc.ch>
2020-02-26 13:57:00 +0100

    creation projet

---
+++ kaude/dfts/src/main/java/ch/hearc/dfts/DftsApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.dfts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DftsApplication.class, args);
+	}
+
+}
---
+++ kaude/dfts/src/test/java/ch/hearc/dfts/DftsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hearc.dfts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DftsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
