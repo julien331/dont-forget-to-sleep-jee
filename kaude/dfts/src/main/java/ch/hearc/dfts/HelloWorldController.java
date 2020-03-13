ad7878ccae4b4d011f1f2ef6488352c0baf3cd28
Julien Chappuis <julien.chappuis@he-arc.ch>
2020-03-13 11:08:52 +0100

    tentative support sonarcloud

---
+++ kaude/dfts/src/main/java/ch/hearc/dfts/HelloWorldController.java
@@ -0,0 +1,17 @@
+package ch.hearc.dfts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+	
+	@GetMapping("/hello")
+	public String helloWorld() {
+
+		return "King food";
+	}
+}
