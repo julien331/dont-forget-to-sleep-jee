d0df7294fcccf77ae3a38908ad0f47cad5f4ca1b
Julien Chappuis <julien.chappuis@he-arc.ch>
2020-03-18 15:40:41 +0100

    ajout du homecontroller et d'une page d'accueil par défaut

---
+++ kaude/dfts/src/main/java/ch/hearc/dfts/controller/TaskController.java
@@ -0,0 +1,11 @@
+package ch.hearc.dfts.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+
+}
