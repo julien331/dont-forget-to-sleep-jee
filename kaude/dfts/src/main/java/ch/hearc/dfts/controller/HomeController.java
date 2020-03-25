07fccc813698bd49fcdfdbe271205e1b6100c82c
Téo Schaffner <teo.schaffner@he-arc.ch>
2020-03-25 15:30:44 +0100

    design page de liste des tâches ok

--- kaude/dfts/src/main/java/ch/hearc/dfts/controller/HomeController.java
+++ kaude/dfts/src/main/java/ch/hearc/dfts/controller/HomeController.java
@@ -1,15 +1,31 @@
 package ch.hearc.dfts.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import ch.hearc.dfts.models.Task;
+import ch.hearc.dfts.models.UserDetailServiceImpl;
+import ch.hearc.dfts.models.repositories.TaskRepository;
 
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	TaskRepository taskRepo;
+	@Autowired
+	private UserDetailServiceImpl utilisateurService;
+	
 	@RequestMapping(value="/",method=RequestMethod.GET)
-	public String mainPage() {	
+	public String mainPage(Model model) {
+		
+		Iterable<Task> task_list = taskRepo.findAll();
+		model.addAttribute("task_list", task_list);
+		//model.addAttribute("user", utilisateurService.loadCurrentUser());
+		
 		return "index";
 	}
 	
