010226f0dbce58deded8d7000546197f8656ceaa
Téo Schaffner <teo.schaffner@he-arc.ch>
2020-04-10 10:40:16 +0200

    marche pas

---
+++ kaude/dfts/src/test/java/ch/hearc/dfts/controller/HomeControllerTest.java
@@ -0,0 +1,17 @@
+package ch.hearc.dfts.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
